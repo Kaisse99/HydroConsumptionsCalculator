801c4e227eba8089a7f54db42d6d252c1f4c6e8b
Mykyta Varnikov <varnikov@sheridancollege.ca>
2025-04-01 17:57:34 -0400

    Initial Commit

---
+++ A3Mykyta/A3MykytaApp.swift
@@ -0,0 +1,17 @@
+//
+//  A3MykytaApp.swift
+//  A3Mykyta
+//
+//  Created by Mykyta Kaisenberg on 2025-04-01.
+//
+
+import SwiftUI
+
+@main
+struct A3MykytaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A3Mykyta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A3Mykyta
+//
+//  Created by Mykyta Kaisenberg on 2025-04-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
