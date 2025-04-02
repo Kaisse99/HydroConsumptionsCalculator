eb03712a1688e3b803ab4eecbafcffcb2f68a411
Mykyta Varnikov <varnikov@sheridancollege.ca>
2025-04-01 21:40:22 -0400

    Added Form and NavigationView, finished

--- A3Mykyta/ContentView.swift
+++ A3Mykyta/ContentView.swift
@@ -2,22 +2,25 @@
 //  ContentView.swift
 //  A3Mykyta
 //
-//  Created by Mykyta Kaisenberg on 2025-04-01.
+//  Created by Mykyta Varnikov on 2025-04-01.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    // user input variables
     @State var onPeakUse : Double?
     @State var offPeakUse: Double?
     @State var midPeakUse: Double?
     
+    // rates
     private let onPeakRate: Double = 0.132
     private let offPeakRate: Double = 0.065
     private let midPeakRate: Double = 0.094
     private let hstRate: Double = 0.13
     private let rebateRate = 0.08
     
+    // computed properties
     var onPeakCharge: Double {
         (onPeakUse ?? 0.0) * onPeakRate
     }
@@ -44,8 +47,7 @@ struct ContentView: View {
         totalCharges + totalRegulatoryCharge
     }
     
-    
-    
+    // to format user input
     let formatter : NumberFormatter = {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
@@ -53,35 +55,35 @@ struct ContentView: View {
         formatter.maximumFractionDigits = 2
         return formatter
     }()
+    
     var body: some View {
-        
-        Text("Mykyta Varnikov").bold()
-        Section(header: Text("Usage Details")) {
-            TextField("Enter On-Peak usage in kWh", value: $onPeakUse, formatter: formatter ).keyboardType(.decimalPad)
-            Divider()
-            TextField("Enter Off-Peak usage in kWh", value: $offPeakUse, formatter: formatter).keyboardType(.decimalPad)
-            Divider()
-            TextField("Enter Mid-Peak usage in kWh", value: $midPeakUse, formatter: formatter).keyboardType(.decimalPad)
-        }
-        Section(header: Text("Consumption Charges")){
-            Text("On-Peak Charges: $\(onPeakCharge, specifier: "%.2f")")
-            Divider()
-            Text("Off-Peak Charges: $\(offPeakCharge, specifier: "%.2f")")
-            Divider()
-            Text("Mid-Peak Charges: $\(midPeakCharge, specifier: "%.2f")")
-            Divider()
-            Text("Total Consumption Charges: $\(totalCharges, specifier: "%.2f")")
-        }
-        Section(header: Text("Regulatory Charges")){
-            Text("HST (13%): $\(hstCharge, specifier: "%.2f")")
-            Divider()
-            Text("Provincial Rebate (8%): $\(rebateCharge, specifier: "%.2f")")
-            Divider()
-            Text("Total Regulatory Charges: $\(totalRegulatoryCharge, specifier: "%.2f")")
-        }
-        
-        Section(header: Text("Bill amount")){
-            Text("Net Bill Amount: $\(totalBill, specifier: "%.2f")")
+        NavigationView {
+            Form {
+                //User input
+                Section(header: Text("Usage Details")) {
+                    TextField("Enter On-Peak usage in kWh", value: $onPeakUse, formatter: formatter ).keyboardType(.decimalPad)
+                    TextField("Enter Off-Peak usage in kWh", value: $offPeakUse, formatter: formatter).keyboardType(.decimalPad)
+                    TextField("Enter Mid-Peak usage in kWh", value: $midPeakUse, formatter: formatter).keyboardType(.decimalPad)
+                }
+                
+                
+                Section(header: Text("Consumption Charges")){
+                    Text("On-Peak Charges: $\(onPeakCharge, specifier: "%.2f")")
+                    Text("Off-Peak Charges: $\(offPeakCharge, specifier: "%.2f")")
+                    Text("Mid-Peak Charges: $\(midPeakCharge, specifier: "%.2f")")
+                    Text("Total Consumption Charges: $\(totalCharges, specifier: "%.2f")").foregroundColor(.teal).bold()
+                }
+                Section(header: Text("Regulatory Charges")){
+                    Text("HST (13%): $\(hstCharge, specifier: "%.2f")")
+                    Text("Provincial Rebate (8%): $\(rebateCharge, specifier: "%.2f")")
+                    Text("Total Regulatory Charges: $\(totalRegulatoryCharge, specifier: "%.2f")").foregroundColor(.teal).bold()
+                }
+                
+                Section(header: Text("Bill amount")){
+                    Text("Net Bill Amount: $\(totalBill, specifier: "%.2f")").foregroundColor(.red).bold()
+                }
+            }
+            .navigationTitle("Mykyta Varnikov").navigationBarTitleDisplayMode(.inline)
         }
     }
 }
