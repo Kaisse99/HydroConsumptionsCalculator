f5e1175a1a7aabe02c713625b6705054a3019720
Mykyta Varnikov <varnikov@sheridancollege.ca>
2025-04-01 20:26:30 -0400

    made first feature, Usage Details now working

--- A3Mykyta/ContentView.swift
+++ A3Mykyta/ContentView.swift
@@ -8,15 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var onPeakUse : Double?
+    @State var offPeakUse: Double?
+    @State var midPeakUse: Double?
+    
+    let formatter : NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()    
     var body: some View {
         Text("Mykyta Varnikov").bold()
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, word!")
+        
+        Section(header: Text("Usage Details")) {
+            TextField("Enter On-Peak usage in kWh", value: $onPeakUse, formatter: formatter ).keyboardType(.decimalPad)
+            TextField("Enter Off-Peak usage in kWh", value: $offPeakUse, formatter: formatter).keyboardType(.decimalPad)
+            TextField("Enter Mid-Peak usage in kWh", value: $midPeakUse, formatter: formatter).keyboardType(.decimalPad)
         }
-        .padding()
+        Section(header: Text("Consumption Charges")){
+            Text("On-Peak Charges: ")
+            Text("Off-Peak Charges: ")
+            Text("Mid-Peak Charges: ")
+            Text("Total Consumption Charges: ")
+        }
+        Section(header: Text("Regulatory Charges")){
+            Text("HST (13%): ")
+            Text("Provincial Rebate (8%): ")
+            Text("Total Regulatory Charges: ")
+        }
+        
+        Section(header: Text("Bill amount")){
+            Text("Net Bill Amount: ")
+        }
     }
 }
 
